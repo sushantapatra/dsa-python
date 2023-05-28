763948f8509970c3af2730340cb8ba605fed222f
sushanta patra <development.experiment@gmail.com>
2023-05-28 19:39:53 +0530

    add git comment

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
+# https://github.com/sushantapatra/dsa-python.git
+"""
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sushantapatra/covidhospitalapp-django.git
+git push -u origin main
+"""
 print('Welcome to DSA')
